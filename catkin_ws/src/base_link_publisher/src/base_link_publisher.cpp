34c8e0ec5ff031bc6f9daf90b028e10fc67e2d8c
AaronXc <aaronwilcox2000@gmail.com>
2023-01-19 03:00:01 -0330

    remove base_link_publisher duplicate dir

--- catkin_ws/src/base_link_publisher/src/base_link_publisher.cpp
+++ catkin_ws/src/base_link_publisher/src/base_link_publisher.cpp
@@ -10,8 +10,25 @@ int main(int argc, char** argv) {
     ros::Publisher pub = nh.advertise<std_msgs::Int32>("/tf", 1000);
     ros::Rate loop_rate(2);
     
-    std_msgs::Int32 count;
-    count.data = 0;
+  
+    ROSMessages::tf2_msgs::TFMessage base_link(new ROSMessages::tf2_msgs::base_link(TFM));
+	ROSMessages::geometry_msgs::TransformStamped TransformStamped;
+	TransformStamped.header.seq = 0;
+	TransformStamped.header.time = time;
+	TransformStamped.header.frame_id = CurrentParentFrameName;
+	TransformStamped.child_frame_id = CurrentThisFrameName;
+	TransformStamped.transform.translation.x = TranslationX;
+	TransformStamped.transform.translation.y = TranslationY;
+	TransformStamped.transform.translation.z = TranslationZ;
+	TransformStamped.transform.rotation.x = RotationX;
+	TransformStamped.transform.rotation.y = RotationY;
+	TransformStamped.transform.rotation.z = RotationZ;
+	TransformStamped.transform.rotation.w = RotationW;
+
+	TFMessage->transforms.Add(TransformStamped);
+
+	_TFTopic->Publish(base_link);
+
     
     while (ros::ok())
     {
