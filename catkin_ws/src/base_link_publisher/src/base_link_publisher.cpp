84a323db595c92b166902b1436a52f88deab9fad
AaronXc <aaronwilcox2000@gmail.com>
2023-01-19 11:17:13 -0330

    remove aaron directory and make base_link_publisher build

--- catkin_ws/src/base_link_publisher/src/base_link_publisher.cpp
+++ catkin_ws/src/base_link_publisher/src/base_link_publisher.cpp
@@ -7,35 +7,29 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "base_link_publisher");
     ros::NodeHandle nh;
     
-    ros::Publisher pub = nh.advertise<std_msgs::Int32>("/tf", 1000);
-    ros::Rate loop_rate(2);
+    ros::Publisher pub = nh.advertise<geometry_msgs::TFMessage>("/tf", 1000);
+    ros::Rate loop_rate(10);
     
   
-    ROSMessages::tf2_msgs::TFMessage base_link(new ROSMessages::tf2_msgs::base_link(TFM));
-	ROSMessages::geometry_msgs::TransformStamped TransformStamped;
-	TransformStamped.header.seq = 0;
-	TransformStamped.header.time = time;
-	TransformStamped.header.frame_id = CurrentParentFrameName;
-	TransformStamped.child_frame_id = CurrentThisFrameName;
-	TransformStamped.transform.translation.x = TranslationX;
-	TransformStamped.transform.translation.y = TranslationY;
-	TransformStamped.transform.translation.z = TranslationZ;
-	TransformStamped.transform.rotation.x = RotationX;
-	TransformStamped.transform.rotation.y = RotationY;
-	TransformStamped.transform.rotation.z = RotationZ;
-	TransformStamped.transform.rotation.w = RotationW;
-
-	TFMessage->transforms.Add(TransformStamped);
-
-	_TFTopic->Publish(base_link);
-
-    
+    tf2_msgs::TFMessage base_link
+	geometry_msgs::TFStamped transform;
+	base_link.header.seq = 1;
+	base_link.header.time = ros::Time::now();
+	base_link.header.frame_id = "base_link";
+	base_link.child_frame_id = "laser_scan";
+	base_link.transform.translation.x = 0.8;
+	base_link.transform.translation.y =0.0;
+	base_link.transform.translation.z = 0.3;
+	base_link.ransform.rotation.x = 0;
+	base_link.transform.rotation.y = 0;
+	base_link.transform.rotation.z = 0;
+	base_link.transform.rotation.w = 0;
+	    
     while (ros::ok())
     {
-        pub.publish(count);
+        pub.publish(base_link);
         ros::spinOnce();
         loop_rate.sleep();
-        ++count.data;
     }
     
     return 0;
