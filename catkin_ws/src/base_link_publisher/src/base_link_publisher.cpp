324d7de01b5105cf5e269e1b5bc3fa95389d4eb0
AaronXc <aaronwilcox2000@gmail.com>
2023-01-19 12:02:41 -0330

    fix use of TFMessage in base_link_publisher.cpp, add_executable in CMakeLists.txt

--- catkin_ws/src/base_link_publisher/src/base_link_publisher.cpp
+++ catkin_ws/src/base_link_publisher/src/base_link_publisher.cpp
@@ -7,24 +7,26 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "base_link_publisher");
     ros::NodeHandle nh;
     
-    ros::Publisher pub = nh.advertise<geometry_msgs::TFMessage>("/tf", 1000);
+    ros::Publisher pub = nh.advertise<tf2_msgs::TFMessage>("/tf", 1000);
     ros::Rate loop_rate(10);
     
   
-    tf2_msgs::TFMessage base_link
-	geometry_msgs::TFStamped transform;
-	base_link.header.seq = 1;
-	base_link.header.time = ros::Time::now();
-	base_link.header.frame_id = "base_link";
-	base_link.child_frame_id = "laser_scan";
-	base_link.transform.translation.x = 0.8;
-	base_link.transform.translation.y =0.0;
-	base_link.transform.translation.z = 0.3;
-	base_link.ransform.rotation.x = 0;
-	base_link.transform.rotation.y = 0;
-	base_link.transform.rotation.z = 0;
-	base_link.transform.rotation.w = 0;
+    tf2_msgs::TFMessage base_link;
+	geometry_msgs::TransformStamped tfstamped;
+	tfstamped.header.seq = 1;
+	tfstamped.header.stamp = ros::Time::now();
+	tfstamped.header.frame_id = "base_link";
+	tfstamped.child_frame_id = "laser_scan";
+	tfstamped.transform.translation.x = 0.8;
+	tfstamped.transform.translation.y =0.0;
+	tfstamped.transform.translation.z = 0.3;
+	tfstamped.transform.rotation.x = 0;
+	tfstamped.transform.rotation.y = 0;
+	tfstamped.transform.rotation.z = 0;
+	tfstamped.transform.rotation.w = 0;
 	    
+    base_link.transforms[0] = tfstamped;
+
     while (ros::ok())
     {
         pub.publish(base_link);
