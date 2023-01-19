b984d04d7c4b09ab119db7a10a5c4b0170abd703
AaronXc <aaronwilcox2000@gmail.com>
2023-01-19 12:26:57 -0330

    baselink publisher works

--- catkin_ws/src/base_link_publisher/src/base_link_publisher.cpp
+++ catkin_ws/src/base_link_publisher/src/base_link_publisher.cpp
@@ -1,17 +1,18 @@
 #include <ros/ros.h>
 #include <std_msgs/Int32.h>
 #include <tf2_msgs/TFMessage.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 int main(int argc, char** argv) {
 
     ros::init(argc, argv, "base_link_publisher");
     ros::NodeHandle nh;
     
-    ros::Publisher pub = nh.advertise<tf2_msgs::TFMessage>("/tf", 1000);
+    //used to attempt ot publish directly to /tf using ros::Publisher pub = nh.advertise<tf2_msgs::TFMessage>("/tf", 1000);
     ros::Rate loop_rate(10);
     
-  
-    tf2_msgs::TFMessage base_link;
+    tf2_ros::TransformBroadcaster tf2_bdcst;
+    // used to attempt to publish directly to /tf using tf2_msgs::TFMessage base_link;
 	geometry_msgs::TransformStamped tfstamped;
 	tfstamped.header.seq = 1;
 	tfstamped.header.stamp = ros::Time::now();
@@ -25,11 +26,13 @@ int main(int argc, char** argv) {
 	tfstamped.transform.rotation.z = 0;
 	tfstamped.transform.rotation.w = 0;
 	    
-    base_link.transforms[0] = tfstamped;
+    // used to attempt to publish directly to /tf using base_link.transforms[0] = tfstamped;
+    
 
     while (ros::ok())
     {
-        pub.publish(base_link);
+        // used to attempt to publish directly to /tf using pub.publish(base_link);
+        tf2_bdcst.sendTransform(tfstamped);
         ros::spinOnce();
         loop_rate.sleep();
     }
